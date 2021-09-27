e7d6bfd6bafd1afbb6626dea408302bb22f8e6f7
Ankit Devanshi <a.singh725098@gmail.com>
2021-09-27 19:20:00 +0530

    Delete Module4-Solutions directory

--- Module4-Solutions/Module4-Solutions/SpeakGoodBye.js
+++
@@ -1,8 +0,0 @@
-(function(window) {
-    var byeSpeaker = new Object();
-    var speakWord = "Good Bye";
-    byeSpeaker.speak = function speak(name) {
-        console.log(speakWord + " " + name);
-    };
-    window.byeSpeaker = byeSpeaker;
-})(window);
